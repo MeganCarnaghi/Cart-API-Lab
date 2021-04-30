1f36141ba6a2d2644d8795e79270cef32a18af77
Megan <carnagh5@gmail.com>
2021-04-30 09:51:46 -0400

    imported UUID

--- routes.js
+++ routes.js
@@ -3,19 +3,25 @@ const express = require("express");
 // Create new router module
 const routes = express.Router();
 routes.use(express.json());
+// To create a random id
+const { v4: uuidv4 } = require("uuid");
+
+const { response } = require("express");
 
 const cartItems = [
-  { id: 1, product: "bread", price: 2, quantity: 1 },
-  { id: 2, product: "milk", price: 1.5, quantity: 1 },
-  { id: 3, product: "cheese", price: 4, quantity: 2 },
-  { id: 4, product: "eggs", price: 1, quantity: 1 },
+  { id: uuidv4(), product: "bread", price: 2, quantity: 1 },
+  { id: uuidv4(), product: "milk", price: 1.5, quantity: 1 },
+  { id: uuidv4(), product: "cheese", price: 4, quantity: 2 },
+  { id: uuidv4(), product: "eggs", price: 1, quantity: 1 },
 ];
 
 // Define routes
+
+// ROUTE 1
 routes.get("/cart-items", (req, res) => {
-  let maxPrice = req.query.maxPrice;
-  let prefix = req.query.prefix;
-  let pageSize = req.query.pageSize;
+  const maxPrice = req.query.maxPrice;
+  const prefix = req.query.prefix;
+  const pageSize = req.query.pageSize;
 
   if (maxPrice) {
     res.json(cartItems.filter((cartItem) => cartItem.price <= maxPrice));
@@ -27,6 +33,21 @@ routes.get("/cart-items", (req, res) => {
     res.json(cartItems);
   }
   res.status(200);
+});
+
+// ROUTE 2
+//NEED TO FIX
+routes.get("/cart-items/:id", (req, res) => {
+  const itemId = req.params.id;
+  console.log(itemId);
+  const getItem = cartItems.find((cartItem) => cartItem.id === itemId);
+
+  if (!getItem) {
+    response.status(404).send("ID not found.");
+  } else {
+    res.json(getItem);
+    res.status(200);
+  }
 });
 
 // Export router module with routes
