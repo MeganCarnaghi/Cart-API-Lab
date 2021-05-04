08193b2701ab82dd3a2533779c9ca9df8c600b6f
Megan <carnagh5@gmail.com>
2021-05-03 22:06:21 -0400

    updated PUT and POST routes

--- routes.js
+++ routes.js
@@ -41,36 +41,46 @@ router.get("/cart-items/:id", (req, res) => {
   if (!foundItem) {
     res.status(404).send("Cart item not found.");
   } else {
-    res.json(foundItem);
-    res.status(200);
+    res.status(200).json(foundItem);
   }
 });
 
 // ROUTE 3 - POST request to add a new cart item
 router.post("/cart-items", (req, res) => {
-  const newCartItem = {
-    id: uuidv4(),
-    product: req.body.product,
-    price: req.body.price,
-    quantity: req.body.quantity,
-  };
-  cartItems.push(newCartItem);
-  res.json(newCartItem);
-  res.status(201);
+  const quantity = parseInt(req.body.quantity);
+
+  if (!quantity) {
+    res.status(404).send("Quantity needs to be a number.");
+  }
+
+  const price = parseInt(req.body.price);
+
+  if (!price) {
+    res.status(404).send("Price needs to be a number.");
+  } else {
+    const newCartItem = {
+      id: uuidv4(),
+      product: req.body.product,
+      price: req.body.price,
+      quantity: req.body.quantity,
+    };
+    cartItems.push(newCartItem);
+    res.status(201).json(newCartItem);
+  }
 });
 
 // ROUTE 4 - PUT request to update an existing cart item
 router.put("/cart-items/:id", (req, res) => {
-  const itemId = req.params.id;
-  let index = cartItems.findIndex((cartItem) => cartItem.id === itemId);
-  const updatedItem = {
-    id: itemId,
-    product: req.body.product,
-    price: req.body.price,
-    quantity: req.body.quantity,
-  };
-  cartItems[index] = updatedItem;
-  res.status(200);
+  const foundItem = cartItems.find((cartItem) => cartItem.id === req.params.id);
+  if (foundItem) {
+    foundItem.product = req.body.product;
+    foundItem.price = req.body.price;
+    foundItem.quantity = req.body.quantity;
+
+    res.json(foundItem);
+  } else {
+    res.status(404).send("The cart item you're looking to update could not be found.");
+  }
 });
 
 // ROUTE 5 - DELETE request to delete an existing cart item via id
